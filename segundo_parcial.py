01ca4dbf963ece1415f21810ff77822c4f894f90
derliscaceres <derlicaceresn@fpuna.edu.py>
2023-11-06 18:57:42 -0300

    etc

--- segundo_parcial.py
+++ segundo_parcial.py
@@ -1,6 +1,7 @@
 import numpy as np
 import skfuzzy as fuzz
 from skfuzzy import control as ctrl
+import matplotlib.pyplot as plt
 
 # Define the input variables
 rating = ctrl.Antecedent(np.arange(0, 11, 1), 'rating')
@@ -9,26 +10,18 @@ popularity = ctrl.Antecedent(np.arange(0, 11, 1), 'popularity')
 # Define the output variable
 recommendation = ctrl.Consequent(np.arange(0, 11, 1), 'recommendation')
 
-
-
 # Define the membership functions for each variable
 rating['low'] = fuzz.trimf(rating.universe, [0, 0, 5])
 rating['medium'] = fuzz.trimf(rating.universe, [0, 5, 10])
 rating['high'] = fuzz.trimf(rating.universe, [5, 10, 10])
-rating.view()
-input("puto el que lea")
 
 popularity['low'] = fuzz.trimf(popularity.universe, [0, 0, 5])
 popularity['medium'] = fuzz.trimf(popularity.universe, [0, 5, 10])
 popularity['high'] = fuzz.trimf(popularity.universe, [5, 10, 10])
-popularity.view()
-input("puto el que lea")
 
 recommendation['low'] = fuzz.trimf(recommendation.universe, [0, 0, 5])
 recommendation['medium'] = fuzz.trimf(recommendation.universe, [0, 5, 10])
 recommendation['high'] = fuzz.trimf(recommendation.universe, [5, 10, 10])
-recommendation.view()
-input("puto el que lea")
 
 # Define the rules
 rule1 = ctrl.Rule(rating['low'] & popularity['low'], recommendation['low'])
