136891a56b87b5695f2634cfee08e973b1638529
derliscaceres <derlicaceresn@fpuna.edu.py>
2023-11-08 23:14:13 -0300

    etc

--- segundo_parcial.py
+++ segundo_parcial.py
@@ -1,24 +1,50 @@
+import json
 import numpy as np
 import skfuzzy as fuzz
 from skfuzzy import control as ctrl
-import matplotlib.pyplot as plt
-import pandas as pd
-import json
+import time
 
 def parse(path):
-    with open(path, 'r') as file:
+    with open(path, 'rb') as file:
         for line in file:
             yield json.loads(line)
 
-def getDF(path):
-    i = 0
-    df = {}
-    for d in parse(path):
-        df[i] = d
-        i += 1
-    return pd.DataFrame.from_dict(df, orient='index')
+def get_unique_asin_products(path):
+    products = {}
+    for data in parse(path):
+        asin = data['asin']
+        if asin not in products:
+            products[asin] = data
+    return list(products.values())
 
-#df = getDF('Software.json')
+def get_reviews(path):
+    reviews = []
+    for data in parse(path):
+        reviews.append(data)
+    return reviews
+
+def similitud(p1, p2):
+    sp1, sp2 = set(), set()
+    for item in p1['also_view']:
+        sp1.add(item)
+    for item in p1['also_buy']:
+        sp1.add(item)
+    for item in p1['category']:
+        sp1.add(item)
+    for item in p2['also_view']:
+        sp2.add(item)
+    for item in p2['also_buy']:
+        sp2.add(item)
+    for item in p2['category']:
+        sp2.add(item)
+
+    if len(sp2) != 0:
+        x = len(sp1.intersection(sp2)) / len(sp1.union(sp2))
+    else:
+        x = 0
+    
+    return x*10
+
 def calcular_recomendacion(rating_value, similarity_value):
     # Define the input variables
     rating = ctrl.Antecedent(np.arange(0, 6, 1), 'rating')
@@ -32,21 +58,17 @@ def calcular_recomendacion(rating_value, similarity_value):
     rating['Average'] = fuzz.trimf(rating.universe, [2.3, 3, 3])
     rating['Good'] = fuzz.trimf(rating.universe, [2.8, 4, 4])
     rating['Excellent'] = fuzz.trimf(rating.universe, [3.8, 5, 5])
-    #rating.view()
-
+    
     similarity['Poor'] = fuzz.trimf(similarity.universe, [0, 0, 3])
     similarity['Average'] = fuzz.trimf(similarity.universe, [2.5, 5.5, 5.5])
     similarity['Good'] = fuzz.trimf(similarity.universe, [5, 8, 8])
     similarity['Excellent'] = fuzz.trimf(similarity.universe, [7, 10, 10])
-    #similarity.view()
-
-
+    
     recommendation['Not recommend'] = fuzz.trimf(recommendation.universe, [0, 0, 3])
     recommendation['Likely to recommend'] = fuzz.trimf(recommendation.universe, [2.5, 5.5, 5.5])
     recommendation['Recommended'] = fuzz.trimf(recommendation.universe, [5, 8, 8])
     recommendation['Highly Recommended'] = fuzz.trimf(recommendation.universe, [7, 10, 10])
-    #recommendation.view()
-
+    
     # Define the rules
     rule1 = ctrl.Rule(rating['Excellent'] & similarity['Excellent'], recommendation['Highly Recommended'])
     rule2 = ctrl.Rule(rating['Excellent'] & similarity['Good'], recommendation['Highly Recommended'])
@@ -55,9 +77,9 @@ def calcular_recomendacion(rating_value, similarity_value):
     rule5 = ctrl.Rule(rating['Average'] & similarity['Good'], recommendation['Recommended'])
     rule6 = ctrl.Rule(rating['Average'] & similarity['Average'], recommendation['Recommended'])
     rule7 = ctrl.Rule(rating['Excellent'] & similarity['Good'], recommendation['Recommended'])
-    rule8 = ctrl.Rule(rating['Average'] & similarity['Average'], recommendation['Likely to recommend'])
+    rule8 = ctrl.Rule(rating['Average'] & similarity['Average'], recommendation['Likely to Recommend'])
     rule9 = ctrl.Rule(rating['Average'] & similarity['Average'], recommendation['Recommended'])
-    rule10 = ctrl.Rule(rating['Average'] & similarity['Average'], recommendation['Not recommend'])
+    rule10 = ctrl.Rule(rating['Average'] & similarity['Average'], recommendation['Not Recommend'])
     rule11 = ctrl.Rule(rating['Poor'] & similarity['Average'], recommendation['Not recommend'])
     rule12 = ctrl.Rule(rating['Excellent'] & similarity['Good'], recommendation['Recommended'])
 
@@ -75,9 +97,39 @@ def calcular_recomendacion(rating_value, similarity_value):
 
     return recommendation_sim.output['recommendation']
 
+# Cargamos las estructuras
+productos_data = get_unique_asin_products('meta_Software.json')
+reviews_data = get_reviews('Software.json')
+
+# Calculo de ratings
+orevall_scores = {}
+for review in reviews_data:
+    asin = review['asin']
+    if asin in orevall_scores:
+        orevall_scores[asin][0] += review['overall']
+        orevall_scores[asin][1] += 1
+    else:
+        orevall_scores[asin] = [review['overall'], 1]
+for producto in productos_data:
+    asin = producto['asin']
+    if asin in orevall_scores:
+        producto['score'] = orevall_scores[asin][0] / orevall_scores[asin][1]
+    else:
+        producto['score'] = 0.0
+
 '''
-rating_input = 2.5
-similarity_input = 8
-recomendacion = calcular_recomendacion(rating_input, similarity_input)
-print("Recommendation:",recomendacion) 
+x=5000 # Los primeros X productos
+similitudes = {}
+for producto in productos_data[:x]:
+    for producto2 in productos_data[:x]:
+        # O(n^2)
+        if producto['asin'] != producto2['asin']:
+            similitudes[producto['asin']] = [producto2['asin'], similitud(producto, producto2)]
 '''
+x=1000
+for producto in productos_data[:x]:
+    for producto2 in productos_data[:x]:
+        if producto['asin'] != producto2['asin']:
+            k = similitud(producto, producto2)
+            if k > 5.5:
+                x = calcular_recomendacion(producto['score'], k)
--- sin_pandas.py
+++ sin_pandas.py
@@ -23,18 +23,24 @@ def get_reviews(path):
 
 def similitud(p1, p2):
     sp1, sp2 = set(), set()
-    for items in p1['also_view']:
-        sp1.add(items)
-    for items in p1['also_buy']:
-        sp1.add(items)
-    for items in p2['also_view']:
-        sp2.add(items)
-    for items in p2['also_buy']:
-        sp2.add(items)
+    for item in p1['also_view']:
+        sp1.add(item)
+    for item in p1['also_buy']:
+        sp1.add(item)
+    for item in p1['category']:
+        sp1.add(item)
+    for item in p2['also_view']:
+        sp2.add(item)
+    for item in p2['also_buy']:
+        sp2.add(item)
+    for item in p2['category']:
+        sp2.add(item)
+
     if len(sp2) != 0:
         x = len(sp1.intersection(sp2)) / len(sp1.union(sp2))
     else:
         x = 0
+    print(x)
     return x*10
 
 start = time.time()
@@ -58,14 +64,19 @@ for producto in productos_data:
     else:
         producto['score'] = 0.0
 
-for producto in productos_data:
-    for producto2 in productos_data:
+end = time.time()
+tiempo = end - start
+print("Tiempo en cargar datos: ", tiempo)
+start = time.time()
+
+x=5000 # Los primeros X productos
+similitudes = {}
+for producto in productos_data[:x]:
+    for producto2 in productos_data[:x]:
+        # O(n^2)
         if producto['asin'] != producto2['asin']:
-            if similitud(producto, producto2) > 5:
-                print("----------------------------------------")
-                print(producto['asin'], " vs ", producto2['asin'])
-                print(similitud(producto, producto2))
+            similitudes[producto['asin']] = [producto2['asin'], similitud(producto, producto2)]
 
 end = time.time()
-
-print(end - start)
+tiempo = end - start
+print("Tiempo en calcular similaridades: ", tiempo)
